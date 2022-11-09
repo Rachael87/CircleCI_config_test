ca6d9dfda78ccc26313858f75b51c27703f0adf4
rachael <rachael.zhang@hstpathways.com>
2022-11-09 11:06:14 +0800

    help

---
+++ Data/Alias.js
@@ -0,0 +1,6 @@
+var xmlAliasData = "";
+xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlAliasData += '<CatapultAliasFile>';
+xmlAliasData += '    <Map Name=\"CHANGE_THIS_NAME\" Link=\"Pages/Add To Favorites.htm\" ResolvedId=\"1\" />';
+xmlAliasData += '</CatapultAliasFile>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
---
+++ Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637491563699688382'
+});
